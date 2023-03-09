59140c8aa9bd82355751097e2588701580a49cb0
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-03-09 11:52:56 +0530

    file added statement.js

---
+++ statements.js
@@ -0,0 +1,27 @@
+//If- else statement example.
+
+let result = 30;
+
+if (result > 40){
+
+    console.log("You passed the test");
+
+}
+else{
+    
+
+    console.log("You did not pass the test");
+}
+
+
+let votingAge = 18;
+
+if (votingAge >= 16)
+{
+
+    console.log("You are elegible for voting 18+");
+}
+else{
+
+    console.log("You are not elegible for voting below 18");
+}
