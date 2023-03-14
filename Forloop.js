771cff84ec8155c701712ca90202dc49ff90a81d
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-03-14 10:48:30 +0530

    forloop examples

---
+++ Forloop.js
@@ -0,0 +1,26 @@
+//Example 1
+
+for ( let i = 0 ; i <= 3 ; i++)
+{
+
+    console.log("This is line",i);
+
+
+
+}
+
+//Example 2
+
+for ( let i = 0 ; i <= 10 ; i++)
+{
+
+
+    console.log("Yashraj-Dandnaik", i );
+}
+
+
+for ( let index = 1 ; index < 10 ; index++)
+{
+
+    console.log(index);
+}
